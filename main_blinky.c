2ce257930922776881b11da003c7e2c89a47e4ac
Robin Choudhury <brotatos@users.noreply.github.com>
2016-12-07 14:42:47 -0800

    Update main_blinky.c

--- main_blinky.c
+++ main_blinky.c
@@ -1,109 +1,3 @@
-/*
-    FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
-    All rights reserved
-
-    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
-
-    This file is part of the FreeRTOS distribution.
-
-    FreeRTOS is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
-
-    ***************************************************************************
-    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
-    >>!   distribute a combined work that includes FreeRTOS without being   !<<
-    >>!   obliged to provide the source code for proprietary components     !<<
-    >>!   outside of the FreeRTOS kernel.                                   !<<
-    ***************************************************************************
-
-    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
-    link: http://www.freertos.org/a00114.html
-
-    ***************************************************************************
-     *                                                                       *
-     *    FreeRTOS provides completely free yet professionally developed,    *
-     *    robust, strictly quality controlled, supported, and cross          *
-     *    platform software that is more than just the market leader, it     *
-     *    is the industry's de facto standard.                               *
-     *                                                                       *
-     *    Help yourself get started quickly while simultaneously helping     *
-     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
-     *    tutorial book, reference manual, or both:                          *
-     *    http://www.FreeRTOS.org/Documentation                              *
-     *                                                                       *
-    ***************************************************************************
-
-    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
-    the FAQ page "My application does not run, what could be wrong?".  Have you
-    defined configASSERT()?
-
-    http://www.FreeRTOS.org/support - In return for receiving this top quality
-    embedded software for free we request you assist our global community by
-    participating in the support forum.
-
-    http://www.FreeRTOS.org/training - Investing in training allows your team to
-    be as productive as possible as early as possible.  Now you can receive
-    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
-    Ltd, and the world's leading authority on the world's leading RTOS.
-
-    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
-    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
-    compatible FAT file system, and our tiny thread aware UDP/IP stack.
-
-    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
-    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
-
-    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
-    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
-    licenses offer ticketed support, indemnification and commercial middleware.
-
-    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
-    engineered and independently SIL3 certified version for use in safety and
-    mission critical applications that require provable dependability.
-
-    1 tab == 4 spaces!
-*/
-
-/******************************************************************************
- * NOTE 1:  This project provides three demo applications.  A simple blinky
- * style project, a more comprehensive test and demo application, and an
- * lwIP example.  The mainSELECTED_APPLICATION setting in main.c is used to
- * select between the three.  See the notes on using mainSELECTED_APPLICATION
- * in main.c.  This file implements the simply blinky style version.
- *
- * NOTE 2:  This file only contains the source code that is specific to the
- * basic demo.  Generic functions, such FreeRTOS hook functions, and functions
- * required to configure the hardware are defined in main.c.
- ******************************************************************************
- *
- * main_blinky() creates one queue, and two tasks.  It then starts the
- * scheduler.
- *
- * The Queue Send Task:
- * The queue send task is implemented by the prvQueueSendTask() function in
- * this file.  prvQueueSendTask() sits in a loop that causes it to repeatedly
- * block for 200 milliseconds, before sending the value 100 to the queue that
- * was created within main_blinky().  Once the value is sent, the task loops
- * back around to block for another 200 milliseconds...and so on.
- *
- * The Queue Receive Task:
- * The queue receive task is implemented by the prvQueueReceiveTask() function
- * in this file.  prvQueueReceiveTask() sits in a loop where it repeatedly
- * blocks on attempts to read data from the queue that was created within
- * main_blinky().  When data is received, the task checks the value of the
- * data, and if the value equals the expected 100, toggles an LED.  The 'block
- * time' parameter passed to the queue receive function specifies that the
- * task should be held in the Blocked state indefinitely to wait for data to
- * be available on the queue.  The queue receive task will only leave the
- * Blocked state when the queue send task writes to the queue.  As the queue
- * send task writes to the queue every 200 milliseconds, the queue receive
- * task leaves the Blocked state every 200 milliseconds, and therefore toggles
- * the LED every 200 milliseconds.
- */
-
 /* Kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
@@ -155,7 +49,7 @@ void main_blinky( void )
 		xTaskCreate( prvQueueSendTask, "TX", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK_PRIORITY, NULL );
 
 		/* Start the tasks and timer running. */
-		vTaskStartScheduler();
+		//vTaskStartScheduler();
 	}
 
 	/* If all is well, the scheduler will now be running, and the following
@@ -174,27 +68,65 @@ void main_blinky( void )
 		vTaskDelay( 100 );
 	}
 	*/
+	u8 readBuf[32];
+	int i;
+
+	for (i = 0; i < 32; i++)
+	{
+		readBuf[i] = 0x00;
+	}
+
+	for( ;; )
+	{
+		/*
+		while(1)
+		{
+			writeTx(readBuf, 1);
+		}*/
+		while (!dataAvailable())
+		{
+			//readRx(readBuf, sizeof(unsigned char));
+			;
+		}
+		i = 0;
+		while(dataAvailable())
+		{
+
+			stopListening();
+			readRx(readBuf[i++], sizeof(unsigned char));
+			//while(1);
+			//readBuf[0] = getRegByte(RPD);
+			(void)readBuf;
+		}
+		startListening();
+	}
 }
 /*-----------------------------------------------------------*/
-//static int reg = 0;
+
 static void prvQueueSendTask( void *pvParameters )
 {
 	(void)pvParameters;
-	//u16 sendBufferSPI1 = 0xABCD;
+	/*
 	u8 readBuf[32];
+	int i;
+
+	for (i = 0; i < 32; i++)
+	{
+		readBuf[i] = 0;
+	}
+
 	for( ;; )
 	{
-		//readReg(0x00);
-
-		vTaskDelay( 100 );
-		readReg(0x00, readBuf);
-		/*
-		vTaskDelay( 100 );
-		writeStatusRegister(0x00, 0x0F);
-
-		vTaskDelay( 100 );
-		readRX();
-		*/
+		if (dataReady())
+		{
+			readRx(readBuf, sizeof(unsigned long));
+		}
+		else
+		{
+			readBuf[0] = 0;
+			readBuf[1] = 0;
+		}
 	}
+	*/
 }
 /*-----------------------------------------------------------*/
